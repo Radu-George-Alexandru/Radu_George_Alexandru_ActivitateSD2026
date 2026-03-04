e2ecf3375c37f0a6887cbf468ee824e4c60a5a66
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-03-04 18:35:24 +0200

    Rezolvare Seminar 2: Filtrare telefoane

---
+++ Probleme suplimentare/Seminar 2/02 - Filtrare telefoane.c
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct Telefon {
+    int id;
+    int ram;
+    char* producator;
+    float pret;
+    char serie;
+} Telefon;
+
+Telefon copiazaTelefon(Telefon t) {
+    Telefon copie = t;
+    copie.producator = copiazaText(t.producator);
+    return copie;
+}
+
+void copiazaTelefoaneScumpe(Telefon* vector, int nrElemente, float pretMinim,
+    Telefon** rezultat, int* dimensiune) {
+
+    *dimensiune = 0;
+    for (int i = 0; i < nrElemente; i++) {
+        if (vector[i].pret >= pretMinim) {
+            (*dimensiune)++;
+        }
+    }
+
+    *rezultat = NULL;
+    if (*dimensiune > 0) {
+        *rezultat = (Telefon*)malloc(sizeof(Telefon) * (*dimensiune));
+        int k = 0;
+        for (int i = 0; i < nrElemente; i++) {
+            if (vector[i].pret >= pretMinim) {
+                (*rezultat)[k++] = copiazaTelefon(vector[i]);
+            }
+        }
+    }
+}
+
+Telefon getPrimulTelefonByProducator(Telefon* vector, int nrElemente, const char* producator) {
+    Telefon rezultat;
+    rezultat.id = -1;
+    rezultat.producator = NULL;
+
+    for (int i = 0; i < nrElemente; i++) {
+        if (strcmp(vector[i].producator, producator) == 0) {
+            return copiazaTelefon(vector[i]);
+        }
+    }
+    return rezultat;
+}
+
