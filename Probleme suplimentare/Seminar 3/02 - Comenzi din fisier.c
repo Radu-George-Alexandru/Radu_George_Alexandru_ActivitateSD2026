b6c25a784c2b5f1a483c8354be92b1135daa2d16
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-03-11 19:28:32 +0200

    Rezolvare Seminar 3: Comenzi din fisier

---
+++ Probleme suplimentare/Seminar 3/02 - Comenzi din fisier.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct Comanda {
+    int id;
+    int nrProduse;
+    float total;
+    char* numeClient;
+    char* oras;
+    char tip;
+} Comanda;
+
+float calculeazaTotalComenzi(Comanda* vector, int nrComenzi) {
+    float suma = 0;
+    for (int i = 0; i < nrComenzi; i++) {
+        suma += vector[i].total;
+    }
+    return suma;
+}
+
+void dezalocareComenzi(Comanda** vector, int* nrComenzi) {
+    if (*vector != NULL) {
+        for (int i = 0; i < *nrComenzi; i++) {
+            free((*vector)[i].numeClient);
+            free((*vector)[i].oras);
+        }
+        free(*vector);
+        *vector = NULL;
+        *nrComenzi = 0;
+    }
+}
+
