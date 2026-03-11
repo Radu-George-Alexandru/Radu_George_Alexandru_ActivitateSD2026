a7c33987d5a16ca1bcc67c438d4040546b2085f8
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-03-11 19:28:32 +0200

    Rezolvare Seminar 3: Laptopuri din fisier

---
+++ Probleme suplimentare/Seminar 3/03 - Laptopuri din fisier.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct Laptop {
+    int id;
+    int ram;
+    float pret;
+    char* model;
+    char* proprietar;
+    char serie;
+} Laptop;
+
+void afiseazaLaptopuriCuRamPestePrag(Laptop* vector, int nrLaptopuri, int pragRam) {
+    for (int i = 0; i < nrLaptopuri; i++) {
+        if (vector[i].ram > pragRam) {
+            printf("%d %d %.2f %s %s %c\n",
+                vector[i].id, vector[i].ram, vector[i].pret,
+                vector[i].model, vector[i].proprietar, vector[i].serie);
+        }
+    }
+}
+
