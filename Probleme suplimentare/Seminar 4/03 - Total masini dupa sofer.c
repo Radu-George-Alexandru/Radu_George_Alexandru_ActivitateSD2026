f651a946b21c7de90aeb60e29601a893fc50194e
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-03-18 14:30:03 +0200

    Rezolvare Seminar 4: Total masini dupa sofer

---
+++ Probleme suplimentare/Seminar 4/03 - Total masini dupa sofer.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct Masina {
+    int id;
+    int nrUsi;
+    float pret;
+    char* model;
+    char* numeSofer;
+    unsigned char serie;
+} Masina;
+
+typedef struct NodMasina {
+    Masina info;
+    struct NodMasina* next;
+} NodMasina;
+
+float calculeazaPretulMasinilorUnuiSofer(NodMasina* cap, const char* numeSofer) {
+    float suma = 0;
+    while (cap != NULL) {
+        if (strcmp(cap->info.numeSofer, numeSofer) == 0) {
+            suma += cap->info.pret;
+        }
+        cap = cap->next;
+    }
+    return suma;
+}
+
