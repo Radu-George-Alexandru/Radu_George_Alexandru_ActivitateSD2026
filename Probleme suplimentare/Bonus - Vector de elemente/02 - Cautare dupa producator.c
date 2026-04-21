3058640a9f4109b330c9e68b061a927d70e3d957
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-04-21 15:11:10 +0300

    Rezolvare bonus: Cautare dupa producator

---
+++ Probleme suplimentare/Bonus - Vector de elemente/02 - Cautare dupa producator.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct Tableta {
+    int id;
+    int memorie;
+    char* producator;
+    float pret;
+    char serie;
+} Tableta;
+
+Tableta copiazaTableta(Tableta t) {
+    Tableta copie = t;
+    copie.producator = copiazaText(t.producator);
+    return copie;
+}
+
+Tableta getPrimaTabletaDupaProducator(Tableta* vector, int nr, const char* producator) {
+    Tableta rezultat;
+    rezultat.id = -1;
+    rezultat.producator = NULL;
+
+    for (int i = 0; i < nr; i++) {
+        if (strcmp(vector[i].producator, producator) == 0) {
+            return copiazaTableta(vector[i]);
+        }
+    }
+    return rezultat;
+}
+
