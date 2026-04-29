a5fb4a6cd4a0314697956bab65af833664ff6318
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-04-29 14:39:48 +0300

    Rezolvare Seminar 8: Top 3 comenzi

---
+++ Probleme suplimentare/Seminar 8/03 - Top 3 comenzi.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct ComandaOnline {
+    int id;
+    int nrProduse;
+    float total;
+    char* client;
+    char* oras;
+    char tip;
+} ComandaOnline;
+
+typedef struct HeapComenzi {
+    int lungime;
+    int nrVizibile;
+    ComandaOnline* vector;
+} HeapComenzi;
+
+ComandaOnline extrageComandaMaxima(HeapComenzi* heap);
+
+void afiseazaTop3Comenzi(HeapComenzi* heap) {
+    for (int i = 0; i < 3 && heap->nrVizibile > 0; i++) {
+        ComandaOnline c = extrageComandaMaxima(heap);
+        printf("%d %s %.2f\n", c.id, c.client, c.total);
+    }
+}
+
