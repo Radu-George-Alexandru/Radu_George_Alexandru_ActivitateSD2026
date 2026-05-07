0b19a50aa390e9c85e33e136574aa8c12b34fe83
Radu George Alexandru <239670577+Radu-George-Alexandru@users.noreply.github.com>
2026-05-07 11:38:43 +0300

    Rezolvare Seminar 9: Total dupa atribut text

---
+++ Probleme suplimentare/Seminar 9/02 - Total dupa atribut text.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char* copiazaText(const char* text) {
+    if (text == NULL) return NULL;
+    char* copie = (char*)malloc(strlen(text) + 1);
+    strcpy(copie, text);
+    return copie;
+}
+
+typedef struct Masina {
+    int id;
+    int nrUsi;
+    float pret;
+    char* model;
+    char* numeSofer;
+    unsigned char serie;
+} Masina;
+
+typedef struct Nod {
+    Masina info;
+    struct Nod* stanga;
+    struct Nod* dreapta;
+} Nod;
+
+float pretTotalMasiniSoferArbore(Nod* radacina, const char* numeSofer) {
+    if (radacina == NULL) return 0;
+
+    float suma = pretTotalMasiniSoferArbore(radacina->stanga, numeSofer)
+        + pretTotalMasiniSoferArbore(radacina->dreapta, numeSofer);
+
+    if (strcmp(radacina->info.numeSofer, numeSofer) == 0) {
+        suma += radacina->info.pret;
+    }
+    return suma;
+}
+
